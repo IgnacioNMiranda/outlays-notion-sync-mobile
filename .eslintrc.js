ca0ab79ddd71370022f0be828b448bf31bf05034
IgnacioNMiranda <ignacio.miranda@reign.cl>
2023-03-19 18:44:05 -0300

    chore: initial config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native'],
+  rules: {},
+}
