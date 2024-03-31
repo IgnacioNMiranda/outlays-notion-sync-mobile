ce50986af4da62f53d7e600ebce5a87ef867db96
IgnacioNMiranda <i.miranda.f99@gmail.com>
2024-03-31 08:33:49 -0300

    chore: update to expo 49

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(false)
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [['module:react-native-dotenv', { moduleName: '@env', path: '.env' }]],
   }
 }
